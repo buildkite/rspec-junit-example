7df0196aea1bc7321a7aa2fc411dd4049153b4a3
Tim Lucas <t@toolmantim.com>
2017-05-30 17:43:44 +1000

    Take II

--- .buildkite/annotate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-echo "--- :buildkite: Creating annotation"
-
-buildkite-agent artifact download junit-annotation.md
-
-buildkite-agent annotate --context junit --style error < junit-annotation.md
