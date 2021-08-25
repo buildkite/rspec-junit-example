1f70419a127f7b35d397e55c6ebb86f479d19047
Lachlan Donald <lachlan@squareup.com>
2021-08-25 16:45:58 +1000

    Bump ruby to 3.x so that nokogiri installs 😬

--- .buildkite/junit.sh
+++ .buildkite/junit.sh
@@ -8,7 +8,8 @@ echo "--- :junit: Download the junits"
 buildkite-agent artifact download tmp/rspec-*.xml tmp
 
 echo "--- :junit::ruby: Processing the junits"
-docker run --rm -v "$(pwd):/app" ruby:2.4 bash -c "cd /app && gem install nokogiri --quiet --silent && ruby /app/.buildkite/lib/junit.rb /app/tmp/*.xml" > tmp/annotation.md
+docker run --rm -v "$(pwd):/app" ruby:3-slim \
+  bash -c "cd /app && gem install nokogiri --quiet --silent && ruby /app/.buildkite/lib/junit.rb /app/tmp/*.xml" > tmp/annotation.md
 
 echo "--- :buildkite: Creating annotation"
 buildkite-agent annotate --context junit --style error < tmp/annotation.md
