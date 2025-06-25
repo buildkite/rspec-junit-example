cfb156bda9e38e09e306dd61483c14b47ab698a9
Sorcha Abel <sorcha@buildkite.com>
2025-06-25 16:08:23 +1000

    PB-27 fix annotation error

--- .buildkite/junit.sh
+++ .buildkite/junit.sh
@@ -11,5 +11,11 @@ echo "--- :junit::ruby: Processing the junits"
 docker run --rm -v "$(pwd):/app" ruby:3-slim \
   bash -c "cd /app && gem install nokogiri --quiet --silent && ruby /app/.buildkite/lib/junit.rb /app/tmp/*.xml" > tmp/annotation.md
 
+# Check annotation file is not empty
+if [ ! -s tmp/annotation.md ]; then
+  echo "No annotation content generated. Skipping annotation."
+  exit 0
+fi
+
 echo "--- :buildkite: Creating annotation"
 buildkite-agent annotate --context junit --style error < tmp/annotation.md
